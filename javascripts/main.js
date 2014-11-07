472bdc6062cd6983cae61f889d98ec47706fb4e3
Ray Farias <ray@rayfarias.com>
2014-11-06 21:59:33 -1000

    further improvements; removed the need for array storage of changed DOM elements

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,12 @@
 $(function() {
-  var selector = $('input.css_selector'); // cache selector element
+  // cache input element
+  var selector = $('input.css_selector');
+  // clear button
   var clearButton = $('button.clear_button');
-  var last_selected = [];
 
   //selector
   selector.on('keyup', function(e) {
-    // console.log(e.keyCode);
+    // console.log(e.keyCode); // debug
     var code = e.keyCode || e.which;
 
     // reset the playing field
@@ -14,7 +15,6 @@ $(function() {
     // 'enter' key applies .selected to elements
     if (code === 13) {
       $(selector.val()).addClass('selected');
-      last_selected.push($(selector.val()));
     }
     // 'down arrow' triggers clearButton
     if (code === 40) {
@@ -25,8 +25,8 @@ $(function() {
   // clear button
   clearButton.on('click', function (e){
     e.preventDefault();
-    last_selected.forEach(function (e) {
-      e.removeClass('selected');
+    $('.selected').each(function (e) {
+      $(this).removeClass('selected');
     });
   });
 
