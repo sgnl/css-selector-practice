7c729a29be1fd631161a4e70496caa069047fe21
Ray Farias <ray@rayfarias.com>
2014-11-06 19:21:27 -1000

    improve useability by adding clearButton functionality to the 'down arrow'

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,10 +5,16 @@ $(function() {
 
   //selector
   selector.on('keyup', function(e) {
+    // console.log(e.keyCode);
     var code = e.keyCode || e.which;
+    // 'enter' key applies .selected to elements
     if (code === 13) {
       $(selector.val()).addClass('selected');
       last_selected = $(selector.val());
+    }
+    // 'down arrow' triggers clearButton
+    if (code === 40) {
+      clearButton.trigger('click');
     }
   });
 
