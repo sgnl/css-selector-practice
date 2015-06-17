d5836a92c07b51b34ad5f36e6d8d197a779b249f
sgnl <rayrfarias@gmail.com>
2015-06-17 11:48:34 -1000

    add obfsc

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,22 @@
 $(function() {
   // cache input element
-  var selector = $('input.css_selector');
+  var ಠ_ಠ711 = $('input.css_selector');
   // clear button
-  var clearButton = $('button.clear_button');
+  var ಠ_ಠ712 = $('button.clear_button');
 
-  //selector
-  selector.on('keyup', function(e) {
-    // console.log(e.keyCode); // debug
-    var code = e.keyCode || e.which;
+  //ಠ_ಠ711
+  ಠ_ಠ711.on('keyup', function(ಠ_ಠ717) {
+    // console.log(ಠ_ಠ717.keyCode); // debug
+    var ಠ_ಠ713 = ಠ_ಠ717.keyCode || ಠ_ಠ717.which;
 
     // reset the playing field
-    $('.selected').each(function (e) {
+    $('.selected').each(function (ಠ_ಠ717) {
       $(this).removeClass('selected');
     });
 
     // 'enter' key applies .selected to elements
-    if (code === 13) {
-      $(selector.val()).addClass('selected');
+    if (ಠ_ಠ713 === 13) {
+      $(ಠ_ಠ711.val()).addClass('selected');
     }
   });
 
@@ -33,30 +33,30 @@ $(function() {
     text: "Instructions"
   });
 
-  var modalBody = $('<p>');
-  modalBody.html('Type in a <span>CSS</span> selector that want to use in the <span>INPUT</span> box pinned to the top of the page. Press <span>ENTER</span> to <span>execute</span> it. You can use the <span>DOWN ARROW</span> to <span>clear</span> the input box. If successful, the selected element will get a <span>GREEN BACKGROUND COLOR</span>.');
+  var ಠ_ಠ714 = $('<p>');
+  ಠ_ಠ714.html('Type in a <span>CSS</span> ಠ_ಠ711 that want to use in the <span>INPUT</span> box pinned to the top of the page. Press <span>ENTER</span> to <span>execute</span> it. You can use the <span>DOWN ARROW</span> to <span>clear</span> the input box. If successful, the selected element will get a <span>GREEN BACKGROUND COLOR</span>.');
 
-  var modalClose = $('<p>');
-  modalClose.html('Click anywhere to close this pop-up.');
+  var ಠ_ಠ715 = $('<p>');
+  ಠ_ಠ715.html('Click anywhere to close this pop-up.');
 
   // add to DOM
   modalContainer.append(modalHeader.hide());
-  modalContainer.append(modalBody.hide());
-  modalContainer.append(modalClose.hide());
+  modalContainer.append(ಠ_ಠ714.hide());
+  modalContainer.append(ಠ_ಠ715.hide());
   modalParent.append(modalContainer.hide());
   $('body').append(modalParent);
 
   // fancy
   modalContainer.slideDown('slow');
   modalHeader.fadeIn('slow');
-  modalBody.fadeIn('slow');
-  modalClose.fadeIn('slow');
+  ಠ_ಠ714.fadeIn('slow');
+  ಠ_ಠ715.fadeIn('slow');
 
   // add Event to remove Modal
-  var closeModalClicks = -1;
+  var ಠ_ಠ716 = -1;
   $('body').on('click', '.modal', function() {
-    closeModalClicks++;
-    if (closeModalClicks >= 1) {
+    ಠ_ಠ716++;
+    if (ಠ_ಠ716 >= 1) {
       $('.modal').fadeOut();
     }
   });
